f0ca99ef9b68e20060873fa09e919ee06b9e12b9
jacksonguerrer0 <jacksonguerrerovega2@gmail.com>
2023-04-24 14:51:48 -0500

    implement add task and read all tasks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('colors')
 // const { showMenu, pause } = require('./helpers/messages')
-const { showMenu, pause } = require('./helpers/inquirer')
+const { showMenu, pause, addTaskInput } = require('./helpers/inquirer')
+const Tasks = require('./models/tasks')
 
 console.clear()
 
@@ -8,8 +9,20 @@ const main = async() => {
   console.log("Run app".yellow)
   let opt = ''
 
+  const tasks = new Tasks
+
   do {
     opt = await showMenu()
+
+    switch (opt) {
+      case '1':
+        const description = await addTaskInput()
+        tasks.addTask(description)
+        break;
+      case '2':
+        console.log("Tasks: ", tasks._all_tasks)
+        break
+    }
 
     if ( opt !== '0' ) await pause()
   } while (opt !== '0')

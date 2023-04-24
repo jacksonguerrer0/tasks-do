57f8be54766a55f5ca30735c998ff2825f2956d0
jacksonguerrer0 <jacksonguerrerovega2@gmail.com>
2023-04-24 14:51:12 -0500

    add input for add task

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,32 +1,6 @@
 const inquirer = require('inquirer');
+const { questionConfirm, questionMenu, questionAddTask } = require('./definitions')
 // require('colors')
-
-
-const question = [
-  {
-    type: 'list',
-    name: 'TODO',
-    default: 'Default value',
-    message: "What do you want to do?",
-    choices: [
-      {name: '1. Create taks', value: '1'},
-      {name: '2. List taks', value: '2'},
-      {name: '3. List taks complete', value: '3'},
-      {name: '4. List taks pending', value: '4'},
-      {name: '5. Complete taks', value: '5'},
-      {name: '6. Delete taks', value: '6'},
-      {name: '0. Leave', value: '0'},
-  ]
-  }
-]
-
-const questionConfirm = [
-  {
-    type: "input",
-    name: "pause",
-    message: "Press enter to continue"
-  }
-]
 
 const showMenu = async () => {
   console.clear()
@@ -34,19 +8,24 @@ const showMenu = async () => {
   console.log("           Select a option".green)
   console.log("|||||||||||||||||||||||||||||||||||||\n".green)
 
-  const { TODO } = await inquirer.prompt(question)
+  const { TODO } = await inquirer.prompt(questionMenu)
 
   return TODO
 }
-
-
 
 const pause = async () => {
   const { pause } = await inquirer.prompt(questionConfirm)
   return pause
 }
 
+const addTaskInput = async () => {
+  const { description } = await inquirer.prompt(questionAddTask)
+
+  return description
+}
+
 module.exports = {
   showMenu,
-  pause
+  pause,
+  addTaskInput
 }

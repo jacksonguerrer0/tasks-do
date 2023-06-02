01aa903c2d2ce2e2836f56c274e8ea38c76236fe
jacksonguerrer0 <jacksonguerrerovega2@gmail.com>
2023-06-02 17:45:16 -0400

    rename description with title

--- models/task.js
+++ models/task.js
@@ -2,11 +2,11 @@ const { v4: uuidv4 } = require('uuid')
 
 class Task {
   id = uuidv4()
-  description = ''
+  title = ''
   completed_at = null
 
-  constructor ( description) {
-    this.description = description
+  constructor ( title ) {
+    this.title = title
   }
 
 }

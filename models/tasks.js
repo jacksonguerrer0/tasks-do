b90071aeee28e0c3f2f8b3ed889e0e52fe030a1b
jacksonguerrer0 <jacksonguerrerovega2@gmail.com>
2023-06-02 17:45:39 -0400

    add new funtions and refactor code

--- models/tasks.js
+++ models/tasks.js
@@ -1,15 +1,23 @@
 const Task = require("./task")
 
 class Tasks {
-  // Example: { '...uuid...': { id: '...', description: '..' } }
+  // Example: { '...uuid...': { id: '...', title: '..' } }
   _all_tasks = {}
 
   constructor () {
     this._all_tasks = {}
   }
 
-  addTask (description) {
-    const task = new Task(description)
+  deleteTask ( id = '' ) {
+    const task = this._all_tasks[id]
+
+    if(task){
+      delete this._all_tasks[id]
+    }
+  }
+
+  addTask (title) {
+    const task = new Task(title)
     this._all_tasks[task.id] = task
   }
 
@@ -20,7 +28,7 @@ class Tasks {
     })
   }
 
-  get listTasks () {
+  listTasks () {
     const listTasks = []
 
     Object.keys(this._all_tasks).forEach((key) => {
@@ -29,6 +37,42 @@ class Tasks {
     })
 
     return listTasks
+  }
+
+  listTasksComplete () {
+    const taksArr = this.listTasks()
+    let formatTasks = ''
+
+    taksArr.forEach((task, i) => {
+      const enumeration = `${i + 1}`.green
+      const status = task.completed_at ? 'COMPLETED'.green : 'PENDING'.red
+
+      formatTasks += `${enumeration}. ${task.title} | ${status}\n`
+    })
+    return formatTasks
+  }
+
+  listTasksByStatus (complete=true) {
+    const taksArr = this.listTasks()
+    let formatTasks = ''
+    let enumeration = 0
+
+    taksArr.forEach((task, i) => {
+      const status = task.completed_at ? 'COMPLETED'.green : 'PENDING'.red
+
+      if (complete) {
+        if (task.completed_at) {
+          enumeration += 1
+          formatTasks += `${enumeration}. ${task.title} | ${status}\n`
+        }
+      } else {
+        if (!task.completed_at) {
+          enumeration += 1
+          formatTasks += `${enumeration}. ${task.title} | ${status}\n`
+        }
+      }
+    })
+    return formatTasks
   }
 }
 

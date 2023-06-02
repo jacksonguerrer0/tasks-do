7ba136acb5820cff27d37654397666a7453eccf1
jacksonguerrer0 <jacksonguerrerovega2@gmail.com>
2023-06-02 17:45:00 -0400

    add new functionalities

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const { questionConfirm, questionMenu, questionAddTask } = require('./definitions')
+const { questionConfirm, questionMenu, questionAddTask, questionSelectTask, questionConfirmDeleteTask } = require('./definitions')
 // require('colors')
 
 const showMenu = async () => {
@@ -19,13 +19,42 @@ const pause = async () => {
 }
 
 const addTaskInput = async () => {
-  const { description } = await inquirer.prompt(questionAddTask)
+  const { title } = await inquirer.prompt(questionAddTask)
 
-  return description
+  return title
+}
+
+const selectTask = async (listTasks = []) => {
+  const choices = listTasks.map((task, i) => {
+    const enumeration = `${i + 1}. `.green
+
+    return {
+      name: enumeration + `${task.title}`,
+      value: task.id
+    }
+  })
+
+  choices.unshift({
+    name: '0. '.green + 'go back',
+    value: 0
+  })
+
+  questionSelectTask[0].choices = choices
+  const { questionDelete } = await inquirer.prompt(questionSelectTask)
+
+  return questionDelete
+}
+
+const confirmDeleteTask = async () => {
+  const { confirmDelete } = await inquirer.prompt(questionConfirmDeleteTask)
+
+  return confirmDelete
 }
 
 module.exports = {
   showMenu,
   pause,
-  addTaskInput
+  addTaskInput,
+  selectTask,
+  confirmDeleteTask
 }
